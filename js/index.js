ceb748c23eac91403d2565e84c017bb83900046d
Gabriel Pacheco <gabioh2012@gmail.com>
2018-01-26 13:47:07 -0500

    Creacin de filtros´

--- js/index.js
+++ js/index.js
@@ -71,35 +71,40 @@ $("#mostrarTodos").click(function() {
 $("#formulario").submit(function(event) {
   event.preventDefault();
   var rango=$("#rangoPrecio").data("ionRangeSlider");
-   filtro($("#selectCiudad").val(), $("#selectTipo").val(), rango.result.from , rango.result.to  );
+  
+  filtro($("#selectCiudad").val(), $("#selectTipo").val(), rango.result.from , rango.result.to  );
 
 });
 function filtro (ciudad, tipo, rangoD, rangoH){ 
-  if (!ciudad){ciudad="no";}
-  if (!tipo){tipo="no";}
+  if (!ciudad || ciudad== ""){ciudad="no";}
+  if (!tipo || tipo == ""){tipo="no";}
   if (!rangoD){rangoD="no";}
   if (!rangoH){rangoH="no";}
   $("#resultados").html("");
   $.get('server/buscar.php', {Ciudad: ciudad, Tipo:tipo, rangod: rangoD, rangoh:rangoH},function(data) {
+    console.log(data);
 
     var casa = JSON.parse (data);
-    console.log(casa.length)
-    for (var i = 0 ; i < casa.length; i++){
-          console.log(i);
-
+    if (casa.mensaje){$("#resultados").html('<div class="col s12 blue ">'
+                                              
+                                                +casa.mensaje 
+                                                
+                                              +'</div>'
+    )}else{
+    for (var i = 0 ; i < casa.length; i++){      
       $("#resultados").append(''+
         '<div class="col s12 ">'
            +'<div class="card horizontal">'
             +'<div class="row">' 
-            +'<div class="card-image col s6  ">'
-              +'<img src="img/home.jpg">'
+            +'<div class="card-image col s4  ">'
+              +'<img class="responsive-img" src="img/home.jpg">'
             +'</div>'
-            +'<div class="card-stacked col s6">'
+            +'<div class="card-stacked col s8">'
               +'<div class="card-content">'
                 +'<p> <strong>Dirección: </strong>'+casa[i].Direccion+'</p>'
                  +'<p> <strong>Ciudad: </strong>'+casa[i].Ciudad+'</p>'
                  +'<p> <strong>Teléfono: </strong>'+casa[i].Telefono+'</p>'
-                  +'<p> <strong>Codigo Postal: </strong>'+casa[i].Codigo_postal+'</p>'
+                  +'<p> <strong>Codigo Postal: </strong>'+casa[i].Codigo_Postal+'</p>'
                    +'<p> <strong>Tipo: </strong>'+casa[i].Tipo+'</p>'
                     +'<p> <strong>Precio: </strong>'+casa[i].Precio+'</p>'
               +'</div>'
@@ -111,5 +116,6 @@ function filtro (ciudad, tipo, rangoD, rangoH){
           +'<div>'
         +'</div>');
     }
+  }
    });
 }
--- server/buscar.php
+++ server/buscar.php
@@ -1,26 +1,97 @@
 <?php
+/******************** Buscador php *****************************/
+//Programa filtra el archivo data-1.json segun campos en el index
+/*****************************************************************/
+//------------------------- indice--------------------------------
+//1 . - Almaceno el archivo en variale y lo trasformo en Json! 
+//2. - Si en la solictus no existen campos almaceno tosdos los registros  en un arreglo  
+//3.- Almaceno los registros filtrados por CIUDAD , TIPO, PRECIO
+//4.- Almaceno los registros filtrados por solo por  PRECIO
+//5.- Almaceno los registros filtrados por solo por  Ciudad y PRECIO
+//6.- Almaceno los registros filtrados por solo por  TIPO y PRECIO
+//7. SI no se obtubieron resultados Almaceno un mensaje Y/O RETORNO EL RESULTADO 
+/*****************************************************************/
+/*****************************************************************/
 
+	//1 . - Almaceno el archivo en variale y lo trasformo en Json! 
 	$data= file_get_contents("../data-1.json");
 	$casas = json_decode($data, true);
+	$filtrado=[];
+
+	//2. - Si en la solictus no existen campos almaceno tosdos los registros  en un arreglo  
 	if ($_GET['Ciudad']=="no" && $_GET['Tipo']=="no" && $_GET['rangod']=="no"){
 		foreach ($casas as $key => $value) {
 				$filtrado[$key]=$value;
 		}
 	}	
+
+	//3.- Almaceno los registros filtrados por CIUDAD , TIPO, PRECIO
 	if ($_GET['Ciudad']!="no" && $_GET['Tipo']!="no"){
 		$cont=0;
 		foreach ($casas as $key => $value) {
-
-			$precio = floatval(preg_replace(",","", explode ("$", $value['Precio'])[1]));
-
+			$precio = str_replace("$", "", $value['Precio']);
+			$precio = str_replace(",", "", $precio);
+			$precio = floatval($precio);
 			if ($value['Ciudad']==$_GET['Ciudad'] && $value['Tipo']==$_GET['Tipo']	&& $precio >= floatval($_GET['rangod']) && $precio <=  floatval($_GET['rangoh'])  ){
 				$filtrado[$cont]=$value;
 				$cont++;
-
-
 			}
 		}
 
 	}
-	echo  json_encode($filtrado);
+
+	//4.- Almaceno los registros filtrados por solo por  PRECIO
+	if ($_GET['Ciudad']=="no" && $_GET['Tipo']=="no"){
+		$cont=0;
+		foreach ($casas as $key => $value) {
+			$precio = str_replace("$", "", $value['Precio']);
+			$precio = str_replace(",", "", $precio);
+			$precio = floatval($precio);
+			if  ($precio >= floatval($_GET['rangod']) && $precio <=  floatval($_GET['rangoh'])  ){
+				$filtrado[$cont]=$value;
+				$cont++;
+			}
+		}
+
+	}
+
+	//5.- Almaceno los registros filtrados por solo por  Ciudad y PRECIO
+	if ($_GET['Ciudad']!="no" && $_GET['Tipo']=="no"){
+		$cont=0;
+		foreach ($casas as $key => $value) {
+
+			$precio = str_replace("$", "", $value['Precio']);
+			$precio = str_replace(",", "", $precio);
+			$precio = floatval($precio);
+			if ($value['Ciudad']==$_GET['Ciudad'] && $precio >= floatval($_GET['rangod']) && $precio <=  floatval($_GET['rangoh'])  ){
+				$filtrado[$cont]=$value;
+				$cont++;
+			}
+		}
+
+	}
+
+	//6.- Almaceno los registros filtrados por solo por  TIPO y PRECIO
+	if ($_GET['Ciudad']=="no" && $_GET['Tipo']!="no"){
+		$cont=0;
+		foreach ($casas as $key => $value) {
+
+			$precio = str_replace("$", "", $value['Precio']);
+			$precio = str_replace(",", "", $precio);
+			$precio = floatval($precio);
+			if ($value['Tipo']==$_GET['Tipo'] && $precio >= floatval($_GET['rangod']) && $precio <=  floatval($_GET['rangoh'])  ){
+				$filtrado[$cont]=$value;
+				$cont++;
+			}
+		}
+
+	}
+
+	//7. SI no se obtubieron resultados Almaceno un mensaje Y/O RETORNO EL RESULTADO 
+	if ( !$filtrado ){
+		$filtrado["mensaje"]= "No se encontraron resultados";
+		echo  json_encode($filtrado);
+	}else{
+		echo  json_encode($filtrado);
+	}
 ?> 
